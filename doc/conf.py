e68bbd2dc684f9f24fe8a436fceb3b07cdfde0de
luke-129 <lukee129@outlook.com>
2025-03-27 06:57:07 +0800

    add my own doco

--- doc/conf.py
+++ doc/conf.py
@@ -6,23 +6,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'Week7Sphinx'
+copyright = '2025, luke fyfe'
+author = 'luke fyfe'
 release = '0.1'
-
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', "autodoc2"]
+
+autodoc2_packages = [
+    "example.py"
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
---
+++ doc/example.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("this is line two")
+print("this is line three")
+
+
+
+
+def multiply(a: float, b: float) -> float:
+    """
+    Multiply two numbers.
+
+    :param a: First number.
+    :param b: Second number.
+    :return: The product of a and b.
+    """
+    return a * b
